501aeeced0733c84a51f2cdfaa68ce6a1f2f3e0f
GreatRoot1214 <jotk1214@gmail.com>
2024-08-05 22:11:23 +0900

    24.08.05

---
+++ code60.py
@@ -0,0 +1,7 @@
+# 검사
+_dict = {"재철" : "3월", "건원" : "6월", "태근" : "12월"}
+# print(_dict["준영"])
+print("3월" in _dict.values())
+print("3월" in _dict.keys())
+# print("3월" in _dict) 는 print("3월" in _dict.keys())와 같다
+
